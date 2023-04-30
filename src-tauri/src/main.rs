65f95da68faef4f1fe4b490529a672e47f2c5b79
BlueZeeKing <github@blueish.dev>
2023-04-30 19:42:00 -0400

    Fix performance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use midi2vol_mac::{midi::Connection, vol::Volume};
-use std::thread;
+use midi2vol_mac::{midi, vol::Volume};
+use tauri::ActivationPolicy;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,19 +11,19 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    thread::spawn(|| {
-        let connection = Connection::new(0);
-        let volume = Volume::new(5.0);
+    let volume = Volume::new(5.0);
 
-        for packet in connection.data.iter() {
-            if packet.channel == 1 || packet.channel == 2 {
-                volume.set((packet.val as f32 / 127.0 * 70.0).round() / 10.0)
-            }
-        }
+    let data = midi::new(0, move |packet| {
+        volume.set((packet.val as f32 / 127.0 * 70.0).round() / 10.0)
     });
 
-    tauri::Builder::default()
+    let app = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .setup(|app| {
+            app.set_activation_policy(ActivationPolicy::Accessory);
+            Ok(())
+        });
+
+    app.run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
